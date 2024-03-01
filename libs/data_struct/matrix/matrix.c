d826cf64b5109214cbcd16828e578bedb536ddd5
Uj3nt <krasnobrizh.dmitry@gmail.com>
2024-03-01 17:55:56 +0300

    create function search min/max element in matrix

--- libs/data_struct/matrix/matrix.c
+++ libs/data_struct/matrix/matrix.c
@@ -3,15 +3,15 @@
 
 //размещает в динамической памяти матрицу размером nRows на nCols
 matrix getMemMatrix(int nRows, int nCols) {
-    int **values = (int **) malloc(sizeof(int*) * nRows);
+    int **values = (int **) malloc(sizeof(int *) * nRows);
     for (int i = 0; i < nRows; i++)
         values[i] = (int *) malloc(sizeof(int) * nCols);
-    return (matrix){values, nRows, nCols};
+    return (matrix) {values, nRows, nCols};
 }
 
 //размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols
 matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols) {
-    matrix *ms = (matrix*) malloc(sizeof(matrix) * nMatrices);
+    matrix *ms = (matrix *) malloc(sizeof(matrix) * nMatrices);
     for (int i = 0; i < nMatrices; i++)
         ms[i] = getMemMatrix(nRows, nCols);
     return ms;
@@ -45,7 +45,7 @@ void inputMatrix(matrix *m) {
 
 
 //ввод массива из nMatrices матриц, хранящейся по адресу ms
-void inputMatrices(matrix *ms, int nMatrices)  {
+void inputMatrices(matrix *ms, int nMatrices) {
     for (int i = 0; i < nMatrices; ++i) {
         inputMatrix(&ms[i]);
     }
@@ -92,54 +92,86 @@ void swapColumns(matrix m, int j1, int j2) {
 
 //выполняет сортировку вставками строк матрицы m
 //по неубыванию значения функции criteria применяемой для строк
-void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int*, int)) ;
+void insertionSortRowsMatrixByRowCriteria(matrix m, int (*criteria)(int *, int));
 
 
 //выполняет сортировку выбором столбцов матрицы m
 // по неубыванию значения функции criteria применяемой для столбцов
-void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int*, int)) ;
+void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int *, int));
 
 
 //возвращает значение ’истина’, если
 //матрица m является квадратной, ложь – в противном случае
-bool isSquareMatrix(matrix *m) ;
+bool isSquareMatrix(matrix *m);
 
 
 //возвращает значение ’истина’,
 //если матрицы m1 и m2 равны, ложь – в противном случае
-bool areTwoMatricesEqual(matrix *m1, matrix *m2) ;
+bool areTwoMatricesEqual(matrix *m1, matrix *m2);
 
 
 // возвращает значение ’истина’, если матрица
 //m является единичной, ложь – в противном случае
-bool isEMatrix(matrix *m) ;
+bool isEMatrix(matrix *m);
 
 
 //возвращает значение ’истина’,
 //если матрица m является симметричной, ложь – в противном случае
-bool isSymmetricMatrix(matrix *m) ;
+bool isSymmetricMatrix(matrix *m);
 
 
 //транспонирует квадратную матрицу m
-void transposeSquareMatrix(matrix *m)  ;
+void transposeSquareMatrix(matrix *m);
 
 
 //транспонирует матрицу m
-void transposeMatrix(matrix *m) ;
+void transposeMatrix(matrix *m);
 
 
 //возвращает позицию минимального элемента матрицы m
-position getMinValuePos(matrix m) ;
+position getMinValuePos(matrix m) {
+    int min = m.values[0][0];
+    position min_position = {0, 0};
 
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols; j++) {
+            if (m.values[i][j] < min) {
+                min = m.values[i][j];
+                min_position.rowIndex = i;
+                min_position.colIndex = j;
+            }
+        }
+    }
+
+    return min_position;
+}
 
 //возвращает позицию максимального элемента матрицы m
-position getMaxValuePos(matrix m) ;
+position getMaxValuePos(matrix m) {
+    int max = m.values[0][0];
+    position max_position = {0, 0};
+
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols; j++) {
+            if (m.values[i][j] > max) {
+                max = m.values[i][j];
+                max_position.rowIndex = i;
+                max_position.colIndex = j;
+            }
+        }
+    }
+
+    return max_position;
+}
+
+
+
 
 //возвращает матрицу размера nRows на nCols, построенную из элементов массива a
-matrix createMatrixFromArray(const int *a, int nRows, int nCols) ;
+matrix createMatrixFromArray(const int *a, int nRows, int nCols);
 
 //возвращает указатель на нулевую матрицу массива из nMatrices матриц,
 //размещенных в динамической памяти, построенных из элементов массива a
-matrix *createArrayOfMatrixFromArray(const int *values, size_t nMatrices, size_t nRows, size_t nCols) ;
+matrix *createArrayOfMatrixFromArray(const int *values, size_t nMatrices, size_t nRows, size_t nCols);
 
 
