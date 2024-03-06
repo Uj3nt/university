71a14deb1a6eec66d3863bc3f6f1c253e4af1f5f
Uj3nt <krasnobrizh.dmitry@gmail.com>
2024-03-06 14:38:43 +0300

    add function by task2

--- libs/data_struct/matrix/matrix_ex.c
+++ libs/data_struct/matrix/matrix_ex.c
@@ -3,8 +3,28 @@
 /* Задание 1  Дана квадратная матрица, все элементы которой различны. Поменять местами
 строки, в которых находятся максимальный и минимальный элементы. */
 
-void swapMinMaxRows(matrix *m) {
-    position maxValuePos = getMaxValuePos(*m);
-    position minValuePos = getMinValuePos(*m);
-    swapRows(*m, minValuePos.rowIndex, maxValuePos.rowIndex);
+void swapMinMaxRows(matrix m) {
+    position maxValuePos = getMaxValuePos(m);
+    position minValuePos = getMinValuePos(m);
+    swapRows(m, minValuePos.rowIndex, maxValuePos.rowIndex);
+}
+
+void task1(matrix m) {
+    swapMinMaxRows(m);
+}
+
+/* Задание 2 Упорядочить строки матрицы по неубыванию наибольших элементов строк: */
+
+int GetMaxInRows(int *a, int n) {
+    int max_value = a[0];
+
+    for (int i = 1; i < n; ++i) {
+        max_value = max_value > a[i] ? max_value : a[i];
+    }
+
+    return max_value;
+}
+
+void task2(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, GetMaxInRows);
 }
