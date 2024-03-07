32fb7e17724cd4d6fd91e7e4e2cf4b418b7e2879
Uj3nt <krasnobrizh.dmitry@gmail.com>
2024-03-07 14:49:19 +0300

    add function by task3

--- libs/data_struct/matrix/matrix_ex.c
+++ libs/data_struct/matrix/matrix_ex.c
@@ -1,6 +1,6 @@
 #include "matrix.h"
 
-/* Задание 1  Дана квадратная матрица, все элементы которой различны. Поменять местами
+/* Задание 1.  Дана квадратная матрица, все элементы которой различны. Поменять местами
 строки, в которых находятся максимальный и минимальный элементы. */
 
 void swapMinMaxRows(matrix m) {
@@ -13,7 +13,7 @@ void task1(matrix m) {
     swapMinMaxRows(m);
 }
 
-/* Задание 2 Упорядочить строки матрицы по неубыванию наибольших элементов строк: */
+/* Задание 2. Упорядочить строки матрицы по неубыванию наибольших элементов строк: */
 
 int GetMaxInRows(int *a, int n) {
     int max_value = a[0];
@@ -27,4 +27,21 @@ int GetMaxInRows(int *a, int n) {
 
 void task2(matrix m) {
     insertionSortRowsMatrixByRowCriteria(m, GetMaxInRows);
+}
+
+/* Задание 3. Дана прямоугольная матрица. Упорядочить столбцы матрицы по неубыванию
+минимальных элементов столбцов:
+ */
+int GetMin(int *a, int n) {
+    int min_value = a[0];
+
+    for (int i = 1; i < n; ++i) {
+        min_value = min_value < a[i] ? min_value : a[i];
+    }
+
+    return min_value;
+}
+
+void task3(matrix m) {
+    selectionSortColsMatrixByColCriteria(m, GetMin);
 }
