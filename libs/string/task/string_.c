78d8cf0cef81a24fed6680b36f756a8b1e083390
Uj3nt <krasnobrizh.dmitry@gmail.com>
2024-03-16 18:16:06 +0300

    add strlen functions

---
+++ libs/string/task/string_.c
@@ -0,0 +1,25 @@
+#include "string_.h"
+
+size_t strlen_v1(char *s) {
+    int i = 0;
+    while (s[i] != '\0')
+        i++;
+    return i;
+}
+
+size_t strlen_v2(char *s) {
+    int i = 0;
+    while (*s != '\0') {
+        i++;
+        s++;
+    }
+    return i;
+}
+
+size_t strlen_(const char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
+
